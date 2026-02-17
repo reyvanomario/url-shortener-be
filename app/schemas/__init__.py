cd460d8b47cc173474e3c8950a32194823259623
reyvanomario <reyvanomario24122004@gmail.com>
2026-02-17 20:33:53 +0700

    delete unused import

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,7 +1,7 @@
 from .Url import UrlBase, UrlCreate, UrlResponse
-from .User import UserBase, UserCreate, UserUpdate, UserResponse
+from .User import UserBase, UserCreate, UserResponse
 from .Auth import LoginRequest, LoginResponse
 from .JWTToken import LoginJwtResponse, TokenData
 from .Statistics import StatisticsBase
 
-__all__ = ["UrlBase", "UrlCreate", "UrlResponse", "UserBase", "UserCreate", "UserUpdate", "UserResponse", "LoginRequest", "LoginResponse", "LoginJwtResponse", "TokenData", "StatisticsBase"]
+__all__ = ["UrlBase", "UrlCreate", "UrlResponse", "UserBase", "UserCreate", "UserResponse", "LoginRequest", "LoginResponse", "LoginJwtResponse", "TokenData", "StatisticsBase"]
