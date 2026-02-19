22d9f195f03aa67578615ac7b77fa68c97970e45
reyvanomario <reyvanomario24122004@gmail.com>
2026-02-19 23:22:24 +0700

    cleaning code

--- app/core/config.py
+++ app/core/config.py
@@ -1,7 +1,6 @@
 from pydantic_settings import BaseSettings
 from functools import lru_cache
 from typing import Optional
-import os
 
 class Settings(BaseSettings):
     ENVIRONMENT: str = "development"
